1d97116da8bcbbc4abc10fd3ad4ddfc4bd0c9b7b
Neenu Thankachan <68829141+Neenu2395@users.noreply.github.com>
2026-05-19 23:11:11 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { FAQ } from './pages/FAQ';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
-
-<li><a href="/faq" className="hover:text-brand-gold transition-colors">FAQ</a></li>
