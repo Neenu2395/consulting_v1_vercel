5a91966bbd82b2b741d9449439b8655d4b384d43
Neenu Thankachan <68829141+Neenu2395@users.noreply.github.com>
2026-05-19 23:09:35 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { FAQ } from './pages/FAQ';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
