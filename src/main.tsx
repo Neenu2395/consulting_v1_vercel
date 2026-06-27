e2b470f1834037b7016781b18f13321159c0a951
Neenu Thankachan <68829141+Neenu2395@users.noreply.github.com>
2026-06-27 23:48:48 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,8 @@ import { ViteReactSSG } from 'vite-react-ssg';
 import { routes } from './App';
 import './index.css';
 
-export const createRoot = ViteReactSSG({ routes });
+export const createRoot = ViteReactSSG(
+  { routes },
+  ({ router, app, initialState }) => {},
+  { rootContainer: '#root' }
+);
